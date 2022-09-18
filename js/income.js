835788fce1446231c08bf6820d908f49796219ac
leon-99 <leonzifer@gmail.com>
2022-09-18 11:41:15 +0630

    init

---
+++ js/income.js
@@ -0,0 +1,58 @@
+const months = ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
+let tbody = document.getElementById("tbody")
+let htmlAmount, htmlReinvest, htmlMonth, htmlYear
+
+function invest(amount, month, year, times) {
+    let passive, returnAmount
+    var d = new Date(year, month)
+    result = []
+    let i = 0
+   while (i < times) {
+
+    if (i === 0) {
+        d.setMonth(d.getMonth() + +15)
+       returnAmount = (amount * 3)
+       passive = returnAmount * 0.3
+       returnAmount = returnAmount - passive
+    } else {
+        d = new Date(d.getFullYear(), d.getMonth())
+        d.setMonth(d.getMonth() + +15)
+        returnAmount = Math.round(returnAmount * 3)
+        passive = Math.round(returnAmount * 0.3)
+        returnAmount = returnAmount - passive
+    }
+    result.push({
+        time: i+1,
+        returnIncome: {
+            finishedTime: `${months[d.getMonth()]}, ${d.getFullYear()}`,
+            amount: returnAmount
+        },
+        passiveIncome: {
+            amount: passive
+        }
+    })
+    i++
+   }
+   return result
+}
+
+
+
+function setData() {
+    htmlAmount = document.getElementById("amount").value
+    htmlReinvest = document.getElementById("reInvest").value
+    htmlMonth = document.getElementById("month").value
+    htmlYear = document.getElementById("year").value
+    let data = invest(htmlAmount, htmlMonth, htmlYear, htmlReinvest)
+    data.forEach(i => {
+        tbody.innerHTML += `<tr>
+    <th scope="row">${i.time}</th>
+    <td>${i.returnIncome.amount}</td>
+    <td>${i.returnIncome.finishedTime}</td>
+    <td>${i.passiveIncome.amount}</td>
+    </tr>`
+    });
+}
+
+document.getElementById("submit").addEventListener("click", setData)
